19f4aae6f16903825d70e24aafa2df4a58a06dc5
Aashish Rana <aashishrana71135@gmail.com>
2022-02-27 16:58:53 +0530

    how to take input and inset node in linkedlist

--- Linked List/Linkedlist.cpp
+++ Linked List/Linkedlist.cpp
@@ -2,11 +2,87 @@
 using namespace std;
 #include "Node.cpp"
 
+Node * takeInput() {
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    while(data != -1) {
+        Node *newNode = new Node(data);
+        if(head == NULL) {
+            head = newNode; 
+
+        }
+        else {
+            Node *temp = head;
+            while (temp -> next != NULL)
+            {
+               temp = temp -> next;
+            }
+            temp -> next = newNode;
+            
+        }
+        
+
+        cin >> data;
+    }
+    return head;      /// Time complexity of this function is O(n square);
+
+}
+
+Node *takeInput_Better() {
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else {
+            tail -> next = newNode;
+            tail = tail -> next;
+            // or 
+            // tail = newNode;
+        }
+        cin >> data;
+    }
+
+    return head;     // Time complexity of this function is O(n);
+    
+}
+
+void insertNode(Node *head, int i, int data) {
+    Node *newNode = new Node(data);
+    int count = 0;
+    Node * temp = head;
+    while (count < i-1) 
+    {
+        temp = temp -> next;
+        count++;
+    }
+
+    if(temp != NULL) {
+        Node *a = temp -> next;
+        temp -> next = newNode;
+        newNode -> next = a;
+
+    }
+
+    
+    
+}
+
  void print(Node *head) 
-    {    
-        while (head != NULL) {
-            cout << head -> data <<" ";
-            head = head -> next;
+    {  
+        Node *temp = head;  
+        while (temp != NULL) {
+            cout << temp -> data <<" ";
+            temp = temp -> next;
         }
         
         
@@ -39,7 +115,7 @@ int main() {
    
 
    
-   
+/*   
 
     Node n1(1);
     Node n2(2);
@@ -58,7 +134,18 @@ int main() {
 
     print(head);
 
+    print(head); */
+
+    Node *head = takeInput();
     print(head);
+
+    int i, data;
+    cin >> i >> data;
+    insertNode(head, i, data);
+
+    print(head);
+
+
 
 
 }
---
+++ merge 2-sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr1[3] = {1,3,5};
+    int arr2[3] = {2,4,5}; 
+
+    int arr3[6] = {0};
+    
+    for(int i=0; i<3; i++) {
+        
+    }
+}
